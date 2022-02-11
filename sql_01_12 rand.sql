f83f25408ffc6290dcbe4addda113fb848a65f10
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-02-10 22:28:24 -0600

    initial commit

---
+++ sql_01_12 rand.sql
@@ -0,0 +1,21 @@
+/*
+SQL for Data Professionals: Lunchbreak Lessons
+
+RAND(N)
+N is the seed. Specify it to get a repeatable random number
+Returns value between 0 and 1
+
+To return a value between i and j
+FLOOR(i + RAND() * (j − i))
+
+Sample Database: Sakila - https://dev.mysql.com/doc/sakila/en/
+*/
+
+
+SELECT 
+-- last_name, rand()
+-- last_name, rand(3)
+-- last_name, FLOOR(5 + RAND() * 3) -- i=5, j=8
+last_name, actor_id, FLOOR(1 + rand() * (actor_id - 1))
+FROM
+    actor
