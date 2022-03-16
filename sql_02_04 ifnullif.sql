d9f3a3a6befd998e278de22e3d8dcef5ad1e5549
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-03-16 16:57:43 -0700

    update examples

--- sql_02_04 ifnullif.sql
+++ sql_02_04 ifnullif.sql
@@ -1,1 +1,19 @@
 # ifnull() and nullif()
+
+#ifnull(expr1, expr2)
+select ifnull(435,450); # 435
+select ifnull(NULL,450); # 450
+
+# practical example
+select last_name, picture, email from staff; # returns null picture
+select last_name, ifnull(picture,"https://default.jpg"), email from staff; 
+
+# nullif(expr1, expr2)
+select nullif(335,335); # NULL
+select nullif(335,350); # 335
+
+# practical example
+# div by zero error (sometimes - depends on ERROR_FOR_DIVISION_BY_ZERO
+set @somenumber = 0;
+select 1/@somenumber; 
+select 1/nullif(@somenumber,0);
