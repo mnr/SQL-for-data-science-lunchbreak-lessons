6c0f2e4f26bbaa239ae4555f481af9ac73f1f990
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-03-17 15:54:27 -0700

    initial commit

---
+++ sql_02_05 case.sql
@@ -0,0 +1,20 @@
+# case
+# there is also a case statement for stored procedures
+SELECT last_update,
+    CASE actor_id
+        WHEN 3 THEN first_name
+        WHEN 5 THEN last_name
+        ELSE "neither"
+    END
+FROM
+    actor;
+    
+SELECT last_update,
+    CASE
+        WHEN actor_id = 3 THEN first_name
+        WHEN actor_id = 5 THEN last_name
+        ELSE 'neither'
+    END
+FROM
+    actor;
+    
