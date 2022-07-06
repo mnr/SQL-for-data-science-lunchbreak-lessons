b6e497f8341dda8c629026819b636e0434f6ebb7
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-07-05 20:57:39 -0700

    initial commit

---
+++ sql_02_0x vscode.sql
@@ -0,0 +1,11 @@
+# how to use Visual Studio Code for SQL development
+
+# create a workspace
+
+# extension: install SQLTools for Visual Studio Code
+# (sql mssql doesnt work for mysql)
+# install SQLTools MySQL/MariaDB Driver
+
+# add a connection to an SQL DATABASE
+
+# Extension: SQL Lint
