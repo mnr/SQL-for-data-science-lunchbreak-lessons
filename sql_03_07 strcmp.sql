ce3ad042c0e5677bc424d3856207890b991213dc
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-10-22 19:58:32 -0500

    prepped for presentation

--- sql_03_07 strcmp.sql
+++ sql_03_07 strcmp.sql
@@ -2,13 +2,29 @@
 
 # STILL WORKING ON THIS...
         
+set @firstString := 'bbb';
+set @secondString := 'bbb';
+
 SELECT 
-    CASE STRCMP('brillig', 'arillig')
-        WHEN - 1 THEN ' first smaller'
-        WHEN 0 THEN 'the same'
-        WHEN 1 THEN 'larger'
+    @firstString, @secondString,
+    CASE STRCMP(@firstString, @secondString)
+        WHEN - 1 THEN concat(@firstString, ' is before ', @secondString )
+        WHEN 0 THEN concat(@firstString, ' is equal to ', @secondString )
+        WHEN 1 THEN concat(@firstString, ' is after ', @secondString )
         ELSE 'why are we here?'
     END;
 
 # comparison is by sort order
 # case insensitive
+
+# real example
+SELECT 
+    first_name,
+    CASE STRCMP(first_name, 'nick')
+        WHEN - 1 THEN 'first half'
+        WHEN 0 THEN 'midpoint'
+        WHEN 1 THEN 'second half'
+        ELSE 'why are we here?'
+    END
+FROM
+    actor
