8f38632bf03933eafa4aefdd79d1a6dfdd4ebf80
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-10-23 16:41:03 -0500

    example code

--- sql_03_08 soundex.sql
+++ sql_03_08 soundex.sql
@@ -1,2 +1,13 @@
 # soundex and sounds like
+# soundex maintained by NARA (https://www.archives.gov/research/census/soundex)
 
+select distinct soundex(first_name) as sndx, first_name
+from actor
+order by (sndx)
+
+# sounds like
+# equivalent to soundex(@a) == soundex(@b)
+
+select distinct soundex(first_name) as sndx, first_name
+from actor
+where first_name sounds like "grace"
