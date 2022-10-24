6ea4f0e65d04c64a328f4fccf2224a0eff400564
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-10-24 17:24:41 -0500

    example setup

--- sql_03_10 substr.sql
+++ sql_03_10 substr.sql
@@ -1,3 +1,20 @@
 # substr
 # substring
-# substring_index
+
+# SUBSTR() is a synonym for SUBSTRING()
+select substr("Jabberwocky" from 3)
+# or...
+select substr("Jabberwocky", 3)
+
+
+select substr("Jabberwocky" from 3 for 6)
+# or...
+select substr("Jabberwocky", 3, 6)
+
+# negative starts from end
+select substr("Jabberwocky" from -3)
+select substr("Jabberwocky" from -8 for 6)
+
+# if len < 1 == empty string
+select substr("Jabberwocky" from -8 for -1)
+
