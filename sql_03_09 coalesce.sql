ba66c729b334faa94b6c0cfd857a5217ff0eb87d
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-10-24 17:22:40 -0500

    includes db change code

--- sql_03_09 coalesce.sql
+++ sql_03_09 coalesce.sql
@@ -2,15 +2,23 @@
 
 SELECT 
     COALESCE("This", "That", "another"),
-    COALESCE(NULL, "That", "another"),
     COALESCE(NULL, NULL, "another"),
     COALESCE("This", NULL, "another"),
     COALESCE(null, NULL, null);
 
 # real life problem
-# db has spurious NULL. Need to mark as "Unique"
+# address2 is sometimes NULL...or not
+# Task: change NULL to " "
+# and leave Box # alone
 select address,
     address2, 
-    coalesce(address2, "Unique") as new_2nd
+    coalesce(address2, " ") as new_2nd
 from
     address
+
+# what I did to film table
+update address 
+    set address2 = 
+        if (rand() > .8,
+        concat("Box ",round(rand()*100)),
+        NULL);
