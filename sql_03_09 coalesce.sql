22ee87b4c0b671214db573148d6565784949f020
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-10-24 12:25:45 -0500

    add example

--- sql_03_09 coalesce.sql
+++ sql_03_09 coalesce.sql
@@ -1,6 +1,16 @@
+# COALESCE returns the first non-null value
+
 SELECT 
     COALESCE("This", "That", "another"),
     COALESCE(NULL, "That", "another"),
     COALESCE(NULL, NULL, "another"),
-    COALESCE("This", NULL, "another");
+    COALESCE("This", NULL, "another"),
+    COALESCE(null, NULL, null);
 
+# real life problem
+# db has spurious NULL. Need to mark as "Unique"
+select address,
+    address2, 
+    coalesce(address2, "Unique") as new_2nd
+from
+    address
