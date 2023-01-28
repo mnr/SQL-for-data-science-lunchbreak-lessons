8b4fc044bb00b198ac51a342ece685cbf856b542
Mark Niemann-Ross <mark@niemannross.com>
2023-01-27 18:17:37 -0800

    where does ROUND operate

--- sql_04_09 round floor ceil.sql
+++ sql_04_09 round floor ceil.sql
@@ -2,6 +2,8 @@
 select @oddNum := actor_id / 3,
     floor(@oddNum),
     round(@oddNum),
+    round(10.49),
+    round(10.5),
     ceiling(@oddNum), # aka CEIL()
     truncate(@oddNum,0),
     truncate(@oddNum,3)
