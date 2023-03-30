c86dbebecd93ae50794a95be75fc28a193a2cc04
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-03-30 11:37:01 -0500

    updates for class

--- sql_05_02 intersect.sql
+++ sql_05_02 intersect.sql
@@ -3,8 +3,22 @@ INTERSECT: Combine only those rows which the results
 of two query blocks have in common, 
 omitting any duplicates.
 
-Each of these set operators supports an ALL modifier. When the ALL keyword follows a set operator, this causes duplicates to be included in the result. 
+Each of these set operators supports an ALL modifier.
+ When the ALL keyword follows a set operator, this causes duplicates to be included in the result. 
 
-All three set operators also support a DISTINCT keyword, which suppresses duplicates in the result. Since this is the default behavior for set operators, it is usually not necessary to specify DISTINCT explicitly.
+All three set operators also support a DISTINCT 
+keyword, which suppresses duplicates in the result. 
+Since this is the default behavior for set operators, 
+it is usually not necessary to specify DISTINCT 
+explicitly.
 
+MySQL 8.0.31 and later
 */
+
+select first_name from actor
+INTERSECT
+select first_name from customer
+
+-- INTERSECT -- produces 72 records
+-- INTERSECT DISTINCT -- 72 records (default)
+-- INTERSECT ALL -- 72 records
--- sql_05_03 except.sql
+++ sql_05_03 except.sql
@@ -3,8 +3,23 @@ EXCEPT: For two query blocks A and B,
 return all results from A which are not also present in B, 
 omitting any duplicates.
 
-Each of these set operators supports an ALL modifier. When the ALL keyword follows a set operator, this causes duplicates to be included in the result. 
+Each of these set operators supports an ALL modifier. 
+When the ALL keyword follows a set operator, 
+this causes duplicates to be included in the result. 
 
-All three set operators also support a DISTINCT keyword, which suppresses duplicates in the result. Since this is the default behavior for set operators, it is usually not necessary to specify DISTINCT explicitly.
+All three set operators also support a DISTINCT keyword, 
+which suppresses duplicates in the result. 
+Since this is the default behavior for set operators, 
+it is usually not necessary to specify DISTINCT explicitly.
 
+MySQL 8.0.31 and later
 */
+
+
+select first_name from actor
+EXCEPT
+select first_name from customer
+
+-- INTERSECT -- produces 56 records
+-- INTERSECT DISTINCT -- 56 records (default)
+-- INTERSECT ALL -- 128 records
